a05116e9ee5fc51d730d8a55233d6abad2ce1c56
Muhammed Burak Gormus <burak.gormus@ug.bilkent.edu.tr>
2021-02-07 21:51:21 +0300

    Delete PitchRollYaw.m

--- SoftRoboticsModeling/PitchRollYaw.m
+++
@@ -1,6 +0,0 @@
-%This is the equation 4.41 in Mohammed's thesis.
-function da = PitchRollYaw (t,a,p,q,r)
-    da = [  p + ( ( q * sin(a(1)) + r * cos(a(1)) ) * tan(a(2)) )
-            q * cos(a(1)) - r * sin(a(1))
-          ( q * sin(a(1)) + r * cos(a(1)) ) / cos(a(2)) ];
-end
