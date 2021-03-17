9541e29ea77a534d029232f11d65f54f71b200fa
Muhammed Burak Gormus <burak.gormus@ug.bilkent.edu.tr>
2021-03-17 11:32:46 +0300

    Add files via upload

---
+++ 17thMarch/ConnectedMassSpringDamperEquation.m
@@ -0,0 +1,17 @@
+function dy = ConnectedMassSpringDamperEquation (t,y,Torque,I_vect,k_vect,c_vect)
+    I1 = I_vect(1);
+    I2 = I_vect(2);
+    k1 = k_vect(1);
+    k2 = k_vect(2);
+    c1 = c_vect(1);
+    c2 = c_vect(2);
+    dy = [  y(2)
+            (k2 * (y(3) - y(1)) + c2 * (y(4) - y(2)) - k1 * y(1) - c1 * y(2)) / I1
+            y(4)
+            (Torque - k2 * (y(3) - y(1)) - c2 * (y(4) - y(2))) / I2 ];
+end
+
+            
+        
+
+
